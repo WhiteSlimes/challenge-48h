871e4b48a6a50ebbe7846dca859f8b958bc1f3a8
thomasmlc <43410009+thomasmlc@users.noreply.github.com>
2020-05-15 12:42:04 +0200

    ~Versioning 1.5~

--- script/map.js
+++ script/map.js
@@ -27,4 +27,32 @@ L.circle([44.8633, -0.5667], {
     fillColor: '#f03',
     fillOpacity: 0.2,
     radius: 500
-}).addTo(map).bindPopup('<h3>Chartrons</h3><p>Note: 4/5</p>')
+}).addTo(map).bindPopup('<h3>Chartrons</h3><p>Note: 3/5</p>')
+
+L.circle([44.8501073, -0.5799909], {
+    color: "red",
+    fillColor: "#f03",
+    radius: 500,
+    fillOpacity: 0.2
+}).addTo(map).bindPopup('<h3>Fondaudége</h3><p>Note: 4/5</p>')
+
+L.circle([44.8759425, -0.5483491], {
+    color: "red",
+    fillColor: "#f03",
+    radius: 500,
+    fillOpacity: 0.2
+}).addTo(map).bindPopup('<h3>Bacalan</h3><p>Note: 3/5</p>')
+
+L.circle([44.8373835,-0.5856761], {
+    color: "red",
+    fillColor: "#f03",
+    radius: 500,
+    fillOpacity: 0.2
+}).addTo(map).bindPopup('<h3>Mériadeck</h3><p>Note: 2/5</p>')
+
+L.circle([44.8260812,-0.5822204], {
+    color: "red",
+    fillColor: "#f03",
+    radius: 500,
+    fillOpacity: 0.2
+}).addTo(map).bindPopup('<h3>St-Genés</h3><p>Note: 4/5</p>')
